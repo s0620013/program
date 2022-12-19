1071e061c2f95f8104a08565bb3e40836990e43a
s0620013 <120359597+s0620013@users.noreply.github.com>
2022-12-19 15:12:11 +0900

    Add files via upload

---
+++ easy_buttan.cc
@@ -0,0 +1,33 @@
+//easy_buttan program
+/*おまじない*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*おまじない終わり*/
+
+/*sceneをいじるパブリッククラス*/
+public class ScreenTransition : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+  
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        //level変数を０にして難易度を優しいに
+        public int level = 0;
+        //sceneを２に変更
+        public int scene = 2;
+        //sceneにsceneチェンジする
+        SceneManager.LoadScene("scene");
+    }
+}
---
+++ normal_buttan.cc
@@ -0,0 +1,33 @@
+//normal_buttan program
+/*おまじない*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*おまじない終わり*/
+
+/*sceneをいじるパブリッククラス*/
+public class ScreenTransition : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        //level変数を１にして難易度を普通に
+        public int level = 1;
+        //sceneを２に変更
+        public int scene = 2;
+        //sceneにsceneチェンジする
+        SceneManager.LoadScene("scene");
+    }
+}
